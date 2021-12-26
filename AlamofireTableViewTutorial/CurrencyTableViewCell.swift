2d972177b4b5cbebb733d620dbd0bc207cd06973
IvanNextToJunior <Vanwan@bk.ru>
2021-12-26 13:39:07 +0300

    add tableView & CurrencyTableViewCell

---
+++ AlamofireTableViewTutorial/CurrencyTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyTableViewCell.swift
+//  AlamofireTableViewTutorial
+//
+//  Created by Ivan on 26.12.2021.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+   
+    
+}
