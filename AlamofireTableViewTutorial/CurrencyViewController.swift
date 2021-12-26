2a33551c314d3eedcf72c9a9c4f271d6b10e4a53
IvanNextToJunior <Vanwan@bk.ru>
2021-12-26 14:13:49 +0300

    Update CurrencyViewController.swift

--- AlamofireTableViewTutorial/CurrencyViewController.swift
+++ AlamofireTableViewTutorial/CurrencyViewController.swift
@@ -8,18 +8,34 @@
 import UIKit
 
 class CurrencyViewController: UIViewController {
-   
+    
     @IBOutlet weak private var tableView: UITableView!
     
     private let identifier = "cell"
     private let nib = UINib(nibName: "CurrencyTableViewCell", bundle: nil)
     
+    private var array: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.register(nib, forCellReuseIdentifier: identifier)
-   
+        tableView.dataSource = self
     }
-
-
+    
 }
 
+extension CurrencyViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! CurrencyTableViewCell
+        
+        return cell
+    }
+    
+}
