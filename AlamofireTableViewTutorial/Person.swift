079967e4027c5d869c32dec3887a5ba1df9d2cda
IvanNextToJunior <Vanwan@bk.ru>
2021-12-27 12:02:39 +0300

    add model

---
+++ AlamofireTableViewTutorial/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  AlamofireTableViewTutorial
+//
+//  Created by Ivan on 27.12.2021.
+//
+
+import Foundation
+
+struct Person: Decodable {
+  
+    let name: String
+    let age: Int
+}
