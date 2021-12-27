7c30eb6e699975523aa3e403bf456ee7a6061ec4
IvanNextToJunior <Vanwan@bk.ru>
2021-12-28 01:45:11 +0300

    refactor

--- AlamofireTableViewTutorial/PersonViewController.swift
+++ AlamofireTableViewTutorial/PersonViewController.swift
@@ -14,7 +14,10 @@ class PersonViewController: UIViewController {
     private let identifier = "cell"
     private let nib = UINib(nibName: "PersonTableViewCell", bundle: nil)
     private let model = AlamofireNetworking()
-    private var people: [Person] = []
+   
+    private var people: [Person] {
+        model.predictAge(for: "Ivan")
+    }
     
     
     override func viewDidLoad() {
@@ -26,7 +29,7 @@ class PersonViewController: UIViewController {
     private func setup () {
         tableView.register(nib, forCellReuseIdentifier: identifier)
         tableView.dataSource = self
-        people = model.predictAge(for: "Ivan")
+        
        
         DispatchQueue.main.async {
             self.tableView.reloadData()
@@ -44,9 +47,7 @@ extension PersonViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! PersonTableViewCell
-     
-       
-        
+    
         cell.nameLabel.text = people[indexPath.row].name
         cell.ageLabel.text = String(people[indexPath.row].age)
         return cell
