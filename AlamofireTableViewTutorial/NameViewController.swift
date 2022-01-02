4f9b0d369e31faa6383a34910094758dbcaa26b9
IvanNextToJunior <Vanwan@bk.ru>
2022-01-02 21:02:08 +0300

    add NameViewController

---
+++ AlamofireTableViewTutorial/NameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NameViewController.swift
+//  AlamofireTableViewTutorial
+//
+//  Created by Ivan on 01.01.2022.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
